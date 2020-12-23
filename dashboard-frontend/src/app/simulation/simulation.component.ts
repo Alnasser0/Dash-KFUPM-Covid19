367dc82994053fb9815c3c4c1dcfbc5c69ce9a86
Ajmal <ajmalsyedali@gmail.com>
2020-12-23 17:57:17 +0300

    Update simualtion

--- dashboard-frontend/src/app/simulation/simulation.component.ts
+++ dashboard-frontend/src/app/simulation/simulation.component.ts
@@ -5,26 +5,32 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-simulation',
   templateUrl: './simulation.component.html',
-  styleUrls: ['./simulation.component.scss']
+  styleUrls: ['./simulation.component.scss'],
 })
 export class SimulationComponent implements OnInit {
   framerate = 5;
   finalStep = 1000;
   src: any;
+  isSimulating = false;
 
-  constructor(private covidDataService: CovidDataService, private sanitizer: DomSanitizer) { }
+  constructor(
+    private covidDataService: CovidDataService,
+    private sanitizer: DomSanitizer
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(): void {
+    this.isSimulating = true;
     this.src = null;
-    this.covidDataService.simulate({ framerate: this.framerate, finalStep: this.finalStep })
-      .subscribe(response => {
-        console.log(response);
-        this.src = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(response));
+    this.covidDataService
+      .simulate({ framerate: this.framerate, finalStep: this.finalStep })
+      .subscribe((response) => {
+        this.isSimulating = false;
+        this.src = this.sanitizer.bypassSecurityTrustUrl(
+          URL.createObjectURL(response)
+        );
         console.log(this.src);
       });
   }
-
 }
